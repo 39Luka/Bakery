b7377863d55619ced1b5d73be4f059b86182cef4
silcaclei <silviacachonleiva@gmail.com>
2025-11-05 15:39:04 +0100

    Cambiando imagenes

--- bakery/src/data/productos.js
+++ bakery/src/data/productos.js
@@ -2,26 +2,26 @@ export const productos = [
   {
     nombre: "Pan pizza 4 quesos",
     descripcion: "Pan crujiente con mezcla de cuatro quesos fundidos.",
-    imagen: "https://images.unsplash.com/photo-1604908812263-88d2bcd36f7f?auto=format&fit=crop&w=600&q=80",
+    imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%2Fid%2FOIP.zgadLh3a-Pg1-l_vmAAJjwHaE8%3Fpid%3DApi&f=1&ipt=0d30820806b6f7fa467ff6741b07e088dbd0b204aecb192dfdd1a661744f8cfe&ipo=images",
   },
   {
     nombre: "Pan de jamón serrano",
     descripcion: "Horneado artesanalmente con jamón serrano y aceite de oliva.",
-    imagen: "https://images.unsplash.com/photo-1613471433259-4b2b41d07923?auto=format&fit=crop&w=600&q=80",
+    imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%2Fid%2FOIP.kN7-nhFCgw1Azn9NvHGSHAHaE6%3Fpid%3DApi&f=1&ipt=0c83072784ab6f0e13921cd23e266bbb959427eb228bc8e5e9d57f305aef0ab3&ipo=images",
   },
   {
     nombre: "Pan de ajo",
     descripcion: "Tostado con mantequilla de ajo y perejil fresco.",
-    imagen: "https://images.unsplash.com/photo-1555507036-ab1f4038808a?auto=format&fit=crop&w=600&q=80",
+    imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%2Fid%2FOIP.Gq1MK1oql8GpNBwxvGXcgwHaE7%3Fpid%3DApi&f=1&ipt=3b731a65d9410bebd0458b56fb979bb4730b5eb8c8eedd92b5abc3be2b823509&ipo=images",
   },
   {
     nombre: "Pan integral",
     descripcion: "Hecho con harina integral, suave y saludable.",
-    imagen: "https://images.unsplash.com/photo-1608198093002-ad4e005484bf?auto=format&fit=crop&w=600&q=80",
+    imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstorage.googleapis.com%2Favena-recipes-v2%2FagtzfmF2ZW5hLWJvdHIZCxIMSW50ZXJjb21Vc2VyGICAgMW2rJ8LDA%2F01-03-2021%2F1614632747603.jpeg&f=1&nofb=1&ipt=77d5f4d32ee33a13c8c737b3378f659de8e8c8e55a39a7df207ca8ec24a7e773",
   },
   {
     nombre: "Baguette clásica",
     descripcion: "Crujiente por fuera, suave por dentro.",
-    imagen: "https://images.unsplash.com/photo-1587668178277-295251f900ce?auto=format&fit=crop&w=600&q=80",
+    imagen: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%2Fid%2FOIP.4NRBnqCXB1424ynW0FM9uwAAAA%3Fpid%3DApi&f=1&ipt=fc4ef114629b91096aaeb4dc8feb0175fb0268206d4c6591a9e88f3183894ace&ipo=images",
   },
 ];
