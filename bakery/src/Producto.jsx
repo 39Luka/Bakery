3b26ca1ea1e161b72c057e5088f3b2950fd72314
silcaclei <silviacachonleiva@gmail.com>
2025-11-07 17:12:37 +0100

    haciendo clases en tailwind

--- bakery/src/Producto.jsx
+++ bakery/src/Producto.jsx
@@ -13,23 +13,33 @@
  */
 
 function Producto({ nombre, descripcion, imagen }) {
-
   return (
     <article className="relative w-[286px] bg-white rounded-xl shadow-lg">
       <figure>
-        <img src={imagen} alt={nombre} className="w-full h-[170px] object-cover rounded-t-xl"/>
+        <img
+          src={imagen}
+          alt={nombre}
+          className="w-full h-[170px] object-cover rounded-t-xl"
+        />
 
-        {/* La sobra decorativa que no se muestra al lector de pantallas*/}
-        <div className="absolute w-[90%] h-[50%] top-[52%] left-[5%] bg-gray-300/60 rounded-lg blur-xl" 
-             aria-hidden="true" 
-             role="presentation">
-        </div>
+        {/* Sombra decorativa visual, no accesible */}
+        <div
+          className="absolute w-[90%] h-[50%] top-[52%] left-[5%] bg-gray-300/60 rounded-lg blur-xl"
+          aria-hidden="true"
+          role="presentation"
+        ></div>
 
         <figcaption className="relative p-4 z-10">
-          <h2 className="text-lg font-bold text-primary mb-1">{nombre}</h2>
-          <p className="text-sm text-gray-600 leading-tight">{descripcion}</p>
-        </figcaption>
+          {/* === Título === */}
+          <h2 className="text-base heading-base heading-h2 mb-1">
+            {nombre}
+          </h2>
 
+          {/* === Descripción === */}
+          <p className="text-base text-small text-gray-700 leading-tight">
+            {descripcion}
+          </p>
+        </figcaption>
       </figure>
     </article>
   );
